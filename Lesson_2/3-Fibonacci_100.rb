917315a18765fd4b8abad07a031e89eaf5b2c673
Kirill122 <34004511+Kirill122@users.noreply.github.com>
2025-09-16 14:21:48 +0300

    Add Lesson_2

---
+++ Lesson_2/3-Fibonacci_100.rb
@@ -0,0 +1,12 @@
+# Заполнить массив числами фибоначчи до 100
+
+MAX = 100
+fibo = [0, 1]
+
+loop do
+  nextnum = fibo.last(2).sum
+  break if nextnum > MAX
+  fibo.push(nextnum)
+end
+
+puts fibo.to_s
---
+++ Lesson_2/5-Year_date.rb
@@ -0,0 +1,30 @@
+=begin
+
+Заданы три числа, которые обозначают число, месяц, год (запрашиваем у пользователя). Найти порядковый номер даты, начиная отсчет с начала года. 
+Учесть, что год может быть високосным. (Запрещено использовать встроенные в ruby методы для этого вроде Date#yday или Date#leap?) 
+
+=end
+
+def leap?(year)
+  return true if year % 400 == 0
+  return false if year % 100 == 0
+  year % 4 == 0
+end
+
+def howmanydays(year, month, day)
+  days_of_months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+  days_of_months[1] = 29 if leap?(year)
+
+  day + days_of_months.take(month - 1).sum
+end
+
+puts "Определим порядковый номер введенной даты"
+print "Введите год "
+year = gets.chomp.to_i
+print "Введите номер месяца "
+month = gets.chomp.to_i
+print "Введите день месяца "
+day = gets.chomp.to_i
+
+puts howmanydays(year, month, day)
